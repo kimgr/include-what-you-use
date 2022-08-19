4aa55f452a14a02dee4d1e309b0095a4ce11cd4f
Kim Gräsman <kim.grasman@gmail.com>
2022-08-19 21:20:42 +0200

    HasImplicitConversionConstructor: DesugarImplicit

--- iwyu_ast_util.cc
+++ iwyu_ast_util.cc
@@ -1281,7 +1281,7 @@ const Type* RemoveReferenceAsWritten(const Type* type) {
 }
 
 bool HasImplicitConversionConstructor(const Type* type) {
-  type = RemoveElaboration(type);  // get rid of the class keyword
+  type = DesugarImplicit(type);  // get rid of the class keyword
   if (isa<PointerType>(type))
     return false;  // can't implicitly convert to a pointer
   if (isa<LValueReferenceType>(type) &&
