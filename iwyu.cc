203fbe35c074f389930fb98a5a2491ae40ec810b
Kim Gräsman <kim.grasman@gmail.com>
2022-08-19 21:04:42 +0200

    VisitFunctionDecl: note that DesugarImplicit won't work

--- iwyu.cc
+++ iwyu.cc
@@ -1783,6 +1783,7 @@ class IwyuBaseAstVisitor : public BaseAstVisitor<Derived> {
       return true;
 
     // ...except the return value.
+    // xxx: Can't use DesugarImplicit
     const Type* return_type
         = RemoveElaboration(decl->getReturnType().getTypePtr());
     const bool is_responsible_for_return_type
